3dcfa167c84c66377daba6e17f512b24a3a4d606
cat K <cat@b4udw3rk5.com>
2023-05-21 11:46:37 +0930

    now accepts a quantity argument

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use std::env;
 use rand::Rng;
 
+// generate a single orgone unit
 fn generate_orgone() -> &'static str {
    let orgone = &["█","▌","▐","▖","▗","▘","▙","▚","▛","▜","▝","▞","▟"];
    let mut rng = rand::thread_rng();
@@ -7,9 +9,25 @@ fn generate_orgone() -> &'static str {
    orgone[random_index]
 }
 
+// if a quantity is provided, print that amount of orgone, otherwise print
+// orgone forever
 fn main() {
-   loop {
-      let character = generate_orgone();
-      print!("{}", character);
+   let args: Vec<String> = env::args().collect();
+   let quantity: usize = if args.len() > 1 {
+      args[1].parse().unwrap_or(0)
+   } else {
+      0
+   };
+
+   if quantity > 0 {
+      for _ in 0..quantity {
+         let energy = generate_orgone();
+         print!("{}", energy);
+      }
+   } else {
+      loop {
+         let energy = generate_orgone();
+         print!("{}", energy);
+      }
    }
 }
