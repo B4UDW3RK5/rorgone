8782bd0b726b9c42e077ef12827943e410350224
cat K <cat@b4udw3rk5.com>
2023-05-21 10:52:32 +0930

    now it just generates orgone forever

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,8 @@ use rand::Rng;
 fn main() {
    let characters = ["█","▌","▐","▖","▗","▘","▙","▚","▛","▜","▝","▞","▟"];
    let mut rng = rand::thread_rng();
-   for _ in 0..9 {
+   loop {
       let random_index = rng.gen_range(0..characters.len());
       print!("{}", characters[random_index]);
    }
-   println!();
 }
