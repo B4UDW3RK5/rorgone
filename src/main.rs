ba67dcf945142192ea3a93f8e599cdd503cd1d94
cat K <cat@b4udw3rk5.com>
2023-05-21 11:30:48 +0930

    moved the generator out to it's own function

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 use rand::Rng;
-fn main() {
-   let characters = ["█","▌","▐","▖","▗","▘","▙","▚","▛","▜","▝","▞","▟"];
+
+fn generate_orgone() -> &'static str {
+   let orgone = &["█","▌","▐","▖","▗","▘","▙","▚","▛","▜","▝","▞","▟"];
    let mut rng = rand::thread_rng();
+   let random_index = rng.gen_range(0..orgone.len());
+   orgone[random_index]
+}
+
+fn main() {
    loop {
-      let random_index = rng.gen_range(0..characters.len());
-      print!("{}", characters[random_index]);
+      let character = generate_orgone();
+      print!("{}", character);
    }
 }
